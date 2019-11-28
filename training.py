bb96feba3f177b73bb0cb470ebfd47d3da62d77e
Ofri Kleinfeld <ofri64@gmail.com>
2019-11-28 23:15:09 +0200

    adds training script

---
+++ training.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import torch
+import torch.nn as nn
+from torch.utils import data
+from models import BiLSTM
+from datasets import TokenMapper, SupervisedDataset
+from configs import BiLSTMConfig, TrainingConfig
+from trainers import ModelTrainer
+
+
+if __name__ == '__main__':
+    # define directories for data and configs
+    data_dir = Path("labeled_data")
+    configs_dir = Path("config_files")
+
+    # define paths to datasets
+    train_path = data_dir / "en_ewt-ud-train.conllu"
+    dev_path = data_dir / "en_ewt-ud-dev.conllu"
+
+    # create mapper and Pytorch's dataset objects
+    mapper = TokenMapper()
+    mapper.create_mapping(train_path)
+    train_dataset = SupervisedDataset(train_path, mapper)
+    # dev_dataset = SupervisedDataset(dev_path, mapper)
+
+    # create model
+    bi_lstm_config = BiLSTMConfig().from_json_file(configs_dir / "BiLSTM_config.json")
+    model = BiLSTM(bi_lstm_config, mapper)
+
+    # create trainer object and train
+    train_config = TrainingConfig().from_json_file(configs_dir / "training_config.json")
+    loss_function = nn.CrossEntropyLoss(ignore_index=0)  # index 0 is used for padding
+    trainer = ModelTrainer(model, train_config, loss_function)
+    trainer.train(train_dataset)
