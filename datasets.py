f1de86a5d85fa72be113a8b601e882f24caf3fec
Ofri Kleinfeld <ofri64@gmail.com>
2019-12-01 09:24:50 +0200

    implemeted a feature of computing the maximum length of sentences in the dataset to be used in inference time

--- datasets.py
+++ datasets.py
@@ -8,11 +8,12 @@ class SupervisedDataset(data.Dataset):
     Pytorch's Dataset derived class to create data sample from
     a path to a file
     """
-    def __init__(self, filepath: str, mapper: TokenMapper, sequence_len: int):
+    def __init__(self, filepath: str, mapper: TokenMapper, prune_sample_length: int = None):
         super().__init__()
         self.filepath = filepath
         self.mapper = mapper
-        self.sequence_len = sequence_len
+        self.prune_sample_length = prune_sample_length
+        self.max_sample_length = -1
         self.samples = []
         self.labels = []
 
@@ -26,15 +27,24 @@ class SupervisedDataset(data.Dataset):
                 if line == "\n":  # marks end of sentence
                     self.samples.append(curr_sent)
                     self.labels.append(curr_labels)
+
                     # clear before reading next sentence
                     curr_sent = []
                     curr_labels = []
+
+                    # update max sample length variable
+                    self.max_sample_length = max(self.max_sample_length, len(curr_sent))
 
                 else:  # line for word in a sentence
                     tokens = line.split("\t")
                     word, label = tokens[1], tokens[3]
                     curr_sent.append(word)
                     curr_labels.append(label)
+
+        # if no pruning is desired - define sequence length of sample to maximum in dataset
+        # this is required for actual computation of accuracy during inference
+        if self.prune_sample_length is None:
+            self.prune_sample_length = self.max_sample_length
 
     def __len__(self) -> int:
         # perform lazy evaluation of data loading
@@ -56,11 +66,11 @@ class SupervisedDataset(data.Dataset):
 
         # add pudding or prune in order to gain unified sequence length
         current_sent_length = len(sent_indices)
-        if current_sent_length > self.sequence_len:
-            sent_indices = sent_indices[:self.sequence_len]
-            labels_indices = labels_indices[:self.sequence_len]
+        if current_sent_length > self.prune_sample_length:
+            sent_indices = sent_indices[:self.prune_sample_length]
+            labels_indices = labels_indices[:self.prune_sample_length]
         else:
-            num_padding = self.sequence_len - current_sent_length
+            num_padding = self.prune_sample_length - current_sent_length
             sent_indices = sent_indices + [self.mapper.token_to_idx[PADD]] * num_padding
             labels_indices = labels_indices + [self.mapper.label_to_idx[PADD]] * num_padding
 
